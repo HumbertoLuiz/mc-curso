34155b978a519c3b22da39a7113864733b8a2f3e
HumbertoLuiz <kaos_132@hotmail.com>
2022-05-27 11:01:12 -0300

    Projeto criado

---
+++ src/main/java/io/github/humbertoluiz/mc/McCursoApplication.java
@@ -0,0 +1,13 @@
+package io.github.humbertoluiz.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/humbertoluiz/mc/McCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.humbertoluiz.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
